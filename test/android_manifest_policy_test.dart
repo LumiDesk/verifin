d5fd7311086818075c82a62705c840b6a75a87bf
Talyra42 <talyra42@foxmail.com>
2026-09-14 10:20:44 +0800

    chore: prepare release v1.18.3

--- test/android_manifest_policy_test.dart
+++ test/android_manifest_policy_test.dart
@@ -4,15 +4,17 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('正式 Manifest 明确禁用 Android 系统备份', () {
-    final manifest = File('android/app/src/main/AndroidManifest.xml')
-        .readAsStringSync();
+    final manifest = File(
+      'android/app/src/main/AndroidManifest.xml',
+    ).readAsStringSync();
 
     expect(manifest, contains('android:allowBackup="false"'));
   });
 
   test('用户设计小组件声明为完整的 AppWidgetProvider', () {
-    final manifest = File('android/app/src/main/AndroidManifest.xml')
-        .readAsStringSync();
+    final manifest = File(
+      'android/app/src/main/AndroidManifest.xml',
+    ).readAsStringSync();
 
     expect(manifest, contains('android:name=".UserWidgetProvider"'));
     expect(
